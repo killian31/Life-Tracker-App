4faf139b3bc8965d2818f36fe7961519cd6c6f51
killian31 <80256683+killian31@users.noreply.github.com>
2022-11-10 13:13:58 +0100

    test

---
+++ Life Tracker/Life_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Life_TrackerApp.swift
+//  Life Tracker
+//
+//  Created by Kiki on 10/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Life_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Life TrackerUITests/Life_TrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Life_TrackerUITestsLaunchTests.swift
+//  Life TrackerUITests
+//
+//  Created by Kiki on 10/11/2022.
+//
+
+import XCTest
+
+final class Life_TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
